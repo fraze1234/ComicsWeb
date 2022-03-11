1d278960078c6c62d8650fb008b5f45f394c2fbd
Valenko2 <93951888+Valenko2@users.noreply.github.com>
2022-03-11 23:12:03 +0300

    trtrtr

--- php/index.php
+++ php/index.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+
+include('reg.php');
+
+if (isset($_REQUEST['go'])) {
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    if(!$login || !$password){
+        $error = 'заполните поля ввода !';
+    }
+    if(!$error) {
+        $query = "INSERT INTO `Users` (`id`, `login`, `password` ) VALUES (NULL, '$login', '$password');";
+        mysqli_query($link, $query);
+        echo "вы создали аккаунт";
+    }else{
+        echo $error; 
+        exit; 
+    }
+
+};
+
+
+
+?>
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>регистрация</title>
 </head>
 <body>
     
+<form method="POST">
+    <p> логин: <input type="text" name="login"></p>
+    <p> пароль: <input type="text" name="password"></p>
+    <p><input type="submit" name="go" value="Создать акаунт"></p>
+</form>
+
 </body>
 </html>
